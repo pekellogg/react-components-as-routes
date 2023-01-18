b0a0f8f77c4c0d534c8aa199bb6802a5a31e7595
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-17 16:06:29 -0800

    commit tree 1af54516bfb86602a856e84d2337f85a39b1c226

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import About from "./About";
+import Home from "./Home";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <Navbar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+  </Router>
+  ,
+  document.getElementById("root")
 );
